5bbe7869eddc7d68b42e7c4d67f96b3b9f370514
Andrei Neacșu <andrei.neacsu@ownzones.com>
2021-06-30 19:29:06 +0300

    Rename lib

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const cdfjs = require('./lib');
+const cdfjs = require('./src');
 
 const data = fs.readFileSync('./samples/genesis_3dl2_gim_20040101_v01.cdf');
 // const data = fs.readFileSync('./samples/dawn_helio1day_position_20070928_v01.cdf');
