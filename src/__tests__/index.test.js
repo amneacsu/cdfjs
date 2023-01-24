2694012c6dc92f0b9e3e36179af5a0766e97ceeb
Andrei Neacșu <andrei.neacsu@ownzones.com>
2023-01-24 08:09:06 +0200

    chore: add jest

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const cdfjs = require('../');
+
+it('foos', () => {
+  const data = fs.readFileSync('./samples/genesis_3dl2_gim_20040101_v01.cdf');
+  // const data = fs.readFileSync('./samples/dawn_helio1day_position_20070928_v01.cdf');
+  const out = cdfjs(data);
+  console.log(out);
+});
